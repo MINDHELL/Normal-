0fca14cea7b0dc465361c585a86328926da37d8f
MINDHELL <Tgpurpose640@gmail.com>
2025-03-21 15:01:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
+
 API_ID = "27788368"
 API_HASH = "9df7e9ef3d7e4145270045e5e43e1081"
 BOT_TOKEN = "7692429836:AAHyUFP6os1A3Hirisl5TV1O5kArGAlAEuQ"
 MONGO_URI = "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "rahu"
 CHANNEL_ID = "-1002465297334"  # Your Telegram channel ID for indexing
-OWNER_ID = "6860316927"  # Replace with your Telegram user ID
+OWNER_ID = int("6860316927")  # Replace with your Telegram user ID
 FORCE_SUB_CHANNEL = "@test89p"  # Channel username for force subscription
 DAILY_LIMIT = "10"  # Maximum videos per user per day
 AUTO_DELETE_TIME = "180"  # Time (in seconds) before auto-deleting sent videos
