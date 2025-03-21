e985158338bb4618469f2fab7d142ae378e31343
MINDHELL <Tgpurpose640@gmail.com>
2025-03-21 16:24:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 API_ID = "27788368"
 API_HASH = "9df7e9ef3d7e4145270045e5e43e1081"
 BOT_TOKEN = "7692429836:AAHyUFP6os1A3Hirisl5TV1O5kArGAlAEuQ"
-MONGO_URI = "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URL = "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "rahu"
 CHANNEL_ID = "-1002465297334"  # Your Telegram channel ID for indexing
 OWNER_ID = int("6860316927")  # Replace with your Telegram user ID
